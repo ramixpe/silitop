17fe9ad82279739573bc1433cf05996cd9e3d3b3
ramixpe <ramixpe@yahoo.com>
2026-04-02 21:59:03 +0100

    Polish package metadata for silitop fork

--- setup.py
+++ setup.py
@@ -1,23 +1,28 @@
 from setuptools import setup, find_packages
 
-long_description = 'Performance monitoring CLI tool for Apple Silicon'
+with open("README.md", "r", encoding="utf-8") as fp:
+    long_description = fp.read()
 
 setup(
     name='silitop',
     version='0.1.0',
-    author='Timothy Liu',
-    author_email='tlkh.xms@gmail.com',
+    author='ramixpe',
+    author_email='',
     url='https://github.com/ramixpe/silitop',
     description='Lightweight performance monitoring CLI tool for Apple Silicon',
     long_description=long_description,
     long_description_content_type="text/markdown",
     license='MIT',
     packages=find_packages(),
-        entry_points={
-            'console_scripts': [
-                'silitop = silitop.cli:main',
-                'asitop = asitop.asitop:main'
-            ]
+    entry_points={
+        'console_scripts': [
+            'silitop = silitop.cli:main',
+            'asitop = asitop.asitop:main'
+        ]
+    },
+    project_urls={
+        "Source": "https://github.com/ramixpe/silitop",
+        "Issues": "https://github.com/ramixpe/silitop/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
